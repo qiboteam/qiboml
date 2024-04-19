d229c917762706f712c2cc2247b3fff5831163ef
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-04-19 16:18:54 +0200

    test: small modification to tests

--- tests/test_models_reuploading.py
+++ tests/test_models_reuploading.py
@@ -1,15 +1,15 @@
 from qiboml.models.reuploading.fourier import FourierReuploading
 from qiboml.models.reuploading.u3 import ReuploadingU3
 
-NQUBITS = 3
+NQUBITS = 2
 NLAYERS = 2
-DATADIM = (3,)
+DATADIM = (2,)
 
 
 def test_reuloading_u3():
     model = ReuploadingU3(nqubits=NQUBITS, nlayers=NLAYERS, data_dimensionality=DATADIM)
     init_angles = model.circuit.get_parameters()
-    model.inject_data((0.4, 0.5, 0.6))
+    model.inject_data((0.4, 0.5))
     new_angles = model.circuit.get_parameters()
     assert init_angles != new_angles
 
@@ -19,6 +19,6 @@ def test_reuloading_fourier():
         nqubits=NQUBITS, nlayers=NLAYERS, data_dimensionality=DATADIM
     )
     init_angles = model.circuit.get_parameters()
-    model.inject_data((0.4, 0.5, 0.6))
+    model.inject_data((0.4, 0.5))
     new_angles = model.circuit.get_parameters()
     assert init_angles != new_angles
