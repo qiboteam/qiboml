a6ac851bfc24a3ce2e8ccfeaf314f6a2281c4ba1
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2024-06-13 10:42:19 +0200

    feat: minor refinements to the torch factory

--- src/qiboml/models/pytorch.py
+++ src/qiboml/models/pytorch.py
@@ -2,30 +2,37 @@ import inspect
 
 import torch
 
-import qiboml.models.encoding_decoding as encodings
-from qiboml.models.abstract import QuantumCircuitLayer
+import qiboml.models.ansatze as ans
+import qiboml.models.encoding_decoding as ed
 
 
-def __init__factory(layer):
-    def __init__(cls, *args, **kwargs):
-        nonlocal layer
-        torch.nn.Module.__init__(cls)
-        layer.__init__(cls, *args, **kwargs)
-        cls.register_parameter(
-            layer.__name__,
-            torch.nn.Parameter(torch.as_tensor(cls.circuit.get_parameters())),
-        )
+def _torch_factory(module) -> None:
+    for name, layer in inspect.getmembers(module, inspect.isclass):
+        if layer.__module__ == module.__name__:
 
-    return __init__
+            def __init__(cls, *args, **kwargs):
+                nonlocal layer
+                torch.nn.Module.__init__(cls)
+                layer.__init__(cls, *args, **kwargs)
+                if len(cls.circuit.get_parameters()) > 0:
+                    cls.register_parameter(
+                        layer.__name__,
+                        torch.nn.Parameter(
+                            torch.as_tensor(cls.circuit.get_parameters())
+                        ),
+                    )
+
+            globals()[name] = type(
+                name,
+                (torch.nn.Module, layer),
+                {
+                    "__init__": __init__,
+                    "forward": layer.forward,
+                    "backward": layer.backward,
+                    "__hash__": torch.nn.Module.__hash__,
+                },
+            )
 
 
-for name, layer in inspect.getmembers(encodings, inspect.isclass):
-    if layer.__module__ == encodings.__name__:
-        newcls = type(
-            name,
-            (torch.nn.Module, layer),
-            {
-                "__init__": __init__factory(layer),
-            },
-        )
-        newcls(3)
+for module in (ed, ans):
+    _torch_factory(module)
