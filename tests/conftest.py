044148a1c09045ba6ea0d04ad7f07a6e0ebe1de8
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2024-06-18 17:20:47 +0200

    fix: added available_backends

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,8 +24,17 @@ def backend(backend_name):
     yield NAME2BACKEND[backend_name]
 
 
+AVAILABLE_BACKENDS = []
+for backend_name in BACKENDS:
+    try:
+        _backend = NAME2BACKEND[backend_name]
+        AVAILABLE_BACKENDS.append(backend_name)
+    except (ModuleNotFoundError, ImportError):
+        pass
+
+
 def pytest_generate_tests(metafunc):
     module_name = metafunc.module.__name__
 
     if "backend_name" in metafunc.fixturenames:
-        metafunc.parametrize("backend_name", BACKENDS)
+        metafunc.parametrize("backend_name", AVAILABLE_BACKENDS)
