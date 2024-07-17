474ad93960929706323bfc44a29f7751324ead84
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2024-07-17 20:52:54 +0200

    fix: small fixes to test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,20 +12,20 @@ def test_binary_encoding_layer(backend):
     nqubits = 10
     qubits = np.random.choice(range(nqubits), size=(6,), replace=False)
     layer = ed.BinaryEncodingLayer(nqubits, qubits=qubits, backend=backend)
-    data = np.random.choice([0, 1], size=(len(qubits),))
+    data = backend.cast(np.random.choice([0, 1], size=(len(qubits),)))
     c = layer(data)
     indices = [gate.qubits[0] for gate in c.queue if gate.name == "x"]
-    assert [qubits[i] for i in (data == 1).nonzero()[0]] == indices
+    assert [qubits[i] for i in np.flatnonzero(data == 1)] == indices
 
 
 def test_phase_encoding_layer(backend):
     nqubits = 10
     qubits = np.random.choice(range(nqubits), size=(6,), replace=False)
     layer = ed.PhaseEncodingLayer(nqubits, qubits=qubits, backend=backend)
-    data = np.random.randn(len(qubits))
+    data = backend.cast(np.random.randn(len(qubits)))
     c = layer(data)
     angles = [gate.init_kwargs["theta"] for gate in c.queue if gate.name == "rz"]
-    np.testing.assert_allclose(data, angles)
+    backend.assert_allclose(data, angles)
 
 
 def test_probabilities_layer(backend):
@@ -33,7 +33,7 @@ def test_probabilities_layer(backend):
     qubits = np.random.choice(range(nqubits), size=(4,), replace=False)
     layer = ed.ProbabilitiesLayer(nqubits, qubits=qubits, backend=backend)
     c = random_clifford(nqubits)
-    np.testing.assert_allclose(layer(c), c().probabilities(qubits))
+    backend.assert_allclose(layer(c), c().probabilities(qubits))
 
 
 def test_state_layer(backend):
@@ -41,4 +41,4 @@ def test_state_layer(backend):
     layer = ed.StateLayer(nqubits, backend=backend)
     c = random_clifford(nqubits)
     real, im = layer(c)
-    np.testing.assert_allclose(real + 1j * im, c().state())
+    backend.assert_allclose(real + 1j * im, c().state())
