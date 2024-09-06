fix: loss.backward() runs without errors, gradients to be tested...

src/qiboml/models/pytorch.py
@@ -47,17 +47,12 @@ class QuantumModel(torch.nn.Module):
                 RuntimeError,
                 f"The last layer has to be a `QuantumDecodinglayer`, but is {self.layers[-1]}",
             )
-        self.differentiation = getattr(Diff, self.differentiation)(self.backend)
+        self.differentiation = getattr(Diff, self.differentiation)()
 
     def forward(self, x: torch.Tensor):
         if self.backend.name != "pytorch":
-            breakpoint()
             x = QuantumModelAutoGrad.apply(
-                x,
-                *list(self.parameters()),
-                self.layers,
-                self.backend,
-                self.differentiation,
+                x, self.layers, self.backend, self.differentiation, *self.parameters()
             )
         else:
             x = _run_layers(x, self.layers)
@@ -82,14 +77,15 @@ class QuantumModelAutoGrad(torch.autograd.Function):
     def forward(
         ctx,
         x: torch.Tensor,
-        *parameters,
         layers: list[QuantumCircuitLayer],
         backend,
         differentiation,
+        *parameters,
     ):
         ctx.save_for_backward(x)
         ctx.layers = layers
         ctx.differentiation = differentiation
+        ctx.backend = backend
         x_clone = x.clone().detach().numpy()
         x_clone = backend.cast(x_clone, dtype=x_clone.dtype)
         x_clone = torch.as_tensor(np.array(_run_layers(x_clone, layers)))
@@ -99,5 +95,8 @@ class QuantumModelAutoGrad(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor):
         (x,) = ctx.saved_tensors
-        gradients = ctx.differentiation.evaluate(x, ctx.layers)
-        return *gradients, None, None, None
+        gradients = [
+            torch.as_tensor(grad)
+            for grad in ctx.differentiation.evaluate(x, ctx.layers)
+        ]
+        return grad_output @ gradients[0].T, None, None, None, *gradients

src/qiboml/operations/differentiation.py
@@ -10,27 +10,27 @@ from qiboml.models.abstract import QuantumCircuitLayer, _run_layers
 
 class PSR:
 
-    def __init__(self, backend):
-        self.backend = backend
+    def __init__(self):
         self.scale_factor = 1.0
         self.epsilon = 1e-2
 
     def evaluate(self, x: ndarray, layers: list[QuantumCircuitLayer]):
+        backend = layers[-1].backend
         gradients = []
         for layer in layers:
             if len(layer.parameters) == 0:
                 continue
             parameters_bkup = layer.parameters.copy()
             gradients.append(
-                self.backend.cast(
+                backend.cast(
                     [
                         self._evaluate_for_parameter(
                             x, layers, layer, i, parameters_bkup
                         )
                         for i in range(len(layer.parameters))
                     ],
-                    self.backend.np.float64,
-                )
+                    backend.np.float64,
+                ).reshape(*parameters_bkup.shape)
             )
         return gradients
 