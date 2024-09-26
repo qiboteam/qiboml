feat: various changes to fix gradient flow

src/qiboml/models/abstract.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Generator
 
 from qibo import Circuit
 from qibo.backends import Backend
@@ -34,19 +35,23 @@ class QuantumCircuitLayer(ABC):
 
     @property
     def has_parameters(self):
-        if len(self.parameters) > 0:
+        if len(list(self.parameters)) > 0:
             return True
         return False
 
     @property
-    def parameters(self) -> ndarray:
-        return self.backend.cast(self.circuit.get_parameters(), self.backend.np.float64)
+    def parameters(self) -> Generator[ndarray, ndarray, ndarray]:
+        # return self.backend.cast(self.circuit.get_parameters(), self.backend.precision)
+        return (gate.parameters for gate in self.circuit.trainable_gates)
 
     @parameters.setter
-    def parameters(self, params: ndarray):
-        self._circuit.set_parameters(
-            self.backend.cast(params.ravel(), self.backend.np.float64)
-        )
+    def parameters(self, params: list[ndarray]):
+        # self._circuit.set_parameters(
+        #    params.ravel()
+        # self.backend.cast(params.ravel(), self.backend.np.float64)
+        # )
+        for param, gate in zip(params, self.circuit.trainable_gates):
+            gate.parameters = param
 
     @property
     def circuit(self) -> Circuit:

src/qiboml/models/encoding_decoding.py
@@ -1,7 +1,7 @@
 """Some standard encoding and decoding layers"""
 
 from dataclasses import dataclass
-from typing import Union
+from typing import Generator, Union
 
 import numpy as np
 from qibo import Circuit, gates
@@ -14,7 +14,36 @@ from qiboml.models.abstract import QuantumCircuitLayer
 
 @dataclass
 class QuantumEncodingLayer(QuantumCircuitLayer):
-    pass
+
+    _input_parameters = None
+    _input_parameters_indices = None
+
+    def _get_input_parameters_indices(self) -> list[tuple[int]]:
+        spans = []
+        index = 0
+        for gate in self._get_non_trainable_gates():
+            nparams = len(gate.parameters)
+            spans.append((index, index + nparams))
+            index += nparams
+        return spans
+
+    def _get_non_trainable_gates(self) -> Generator[gates.Gate, gates.Gate, gates.Gate]:
+        return (gate for gate in self.circuit.parametrized_gates if not gate.trainable)
+
+    @property
+    def input_parameters(
+        self,
+    ) -> list[ndarray]:
+        return self._input_parameters
+
+    @input_parameters.setter
+    def input_parameters(self, params: ndarray):
+        self._input_parameters = []
+        for span, gate in zip(
+            self._input_parameters_indices, self._get_non_trainable_gates()
+        ):
+            self._input_parameters.append(params[span[0] : span[1]])
+            gate.parameters = self._input_parameters[-1]
 
 
 @dataclass
@@ -39,10 +68,11 @@ class PhaseEncodingLayer(QuantumEncodingLayer):
     def __post_init__(self):
         super().__post_init__()
         for q in self.qubits:
-            self.circuit.add(gates.RZ(q, theta=0.0))
+            self.circuit.add(gates.RZ(q, theta=0.0, trainable=False))
+        self._input_parameters_indices = self._get_input_parameters_indices()
 
     def forward(self, x: ndarray) -> Circuit:
-        self.parameters = x * self.backend.np.pi
+        self.input_parameters = x * self.backend.np.pi
         return self.circuit
 
 

src/qiboml/models/pytorch.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 from qibo.backends import Backend
 from qibo.config import raise_error
+from sympy.core import parameters
 
 import qiboml.models.encoding_decoding as ed
 from qiboml.models.abstract import QuantumCircuitLayer, _run_layers
@@ -31,26 +32,24 @@ class QuantumModel(torch.nn.Module):
                     RuntimeError,
                     f"Layer \n{layer}\n is using {layer.backend} backend, but {self.backend} backend was expected.",
                 )
-        for layer in self.layers:
-            if len(layer.parameters) > 0:
-                params = (
-                    layer.parameters
-                    if self.backend.name == "pytorch"
-                    else torch.as_tensor(
-                        layer.parameters.tolist(), dtype=self.backend.np.float64
-                    )
-                )
-                params = torch.nn.Parameter(params)
-                setattr(self, layer.__class__.__name__, params)
-                # self.register_parameter(
-                #    layer.__class__.__name__,
-                #    torch.nn.Parameter(params),
-                # )
         if not isinstance(self.layers[-1], ed.QuantumDecodingLayer):
             raise_error(
                 RuntimeError,
                 f"The last layer has to be a `QuantumDecodinglayer`, but is {self.layers[-1]}",
             )
+
+        for layer in self.layers:
+            for i, params in enumerate(layer.parameters):
+                # if self.backend.name != "pytorch":
+                #    params = torch.as_tensor(
+                #        params, dtype=self.backend.precision
+                #    )
+                setattr(
+                    self,
+                    f"{layer.__class__.__name__}_{i}",
+                    torch.nn.Parameter(torch.as_tensor(params)),
+                )
+
         self.differentiation = getattr(Diff, self.differentiation)()
 
     def forward(self, x: torch.Tensor):
@@ -59,8 +58,23 @@ class QuantumModel(torch.nn.Module):
                 x, self.layers, self.backend, self.differentiation, *self.parameters()
             )
         else:
-            breakpoint()
-            x = _run_layers(x, self.layers, list(self.parameters()))
+            # breakpoint()
+            """
+            index = 0
+            circ = self.layers[0](x)
+            parameters = list(self.parameters())
+            for layer in self.layers[1:-1]:
+                if layer.has_parameters and not issubclass(layer.__class__, ed.QuantumEncodingLayer):
+                    layer.parameters = parameters[index]
+                    index += 1
+                circ = layer.forward(circ)
+            #circ.set_parameters([p for param in self.parameters() for p in param])
+            return self.layers[-1](circ)
+            #x = _run_layers(x, self.layers, list(self.parameters()))
+            """
+            for layer in self.layers:
+                x = layer(x)
+
         return x
 
     @property

tests/test_backprop.py
@@ -13,8 +13,8 @@ from qiboml.models import encoding_decoding as ed
 @pytest.mark.parametrize("backend", [JaxBackend(), PyTorchBackend()])
 @pytest.mark.parametrize("differentiation", ["Jax", "PSR"])
 def test_backpropagation(backend, differentiation):
-    nqubits = 5
-    dim = 4
+    nqubits = 3
+    dim = 2
     training_layer = ans.ReuploadingLayer(nqubits, backend=backend)
     encoding_layer = ed.PhaseEncodingLayer(nqubits, backend=backend)
     kwargs = {"backend": backend}
@@ -38,20 +38,22 @@ def test_backpropagation(backend, differentiation):
     encoding = torch.nn.Linear(1, nqubits)
     model = torch.nn.Sequential(encoding, q_model)
     # try to fit a parabola
-    x = torch.randn(10, 1)
+    x = torch.randn(100, 1)
     y = x**2
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1, momentum=0.9)
     for input, target in zip(x, y):
         optimizer.zero_grad()
         output = model(input)
         loss = (target - output) ** 2
-        params_bkp = torch.cat(tuple(p.ravel() for p in model.parameters()))
+        print(list(model.parameters()))
         print(f"> loss: {loss}")
         loss.backward()
         optimizer.step()
-        print(
-            f"> Parameters delta: {torch.cat(tuple(p.ravel() for p in model.parameters())) - params_bkp}"
-        )
+        print(list(model.parameters()))
+
+        # print(
+        #    f"> Parameters delta: {torch.cat(tuple(p.ravel() for p in model.parameters())) - params_bkp}"
+        # )
 
     assert False