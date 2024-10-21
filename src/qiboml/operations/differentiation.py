fix: splitted jacobian with and without gradients

src/qiboml/operations/differentiation.py
@@ -10,6 +10,7 @@ from torch.autograd import forward_ad
 from qiboml import ndarray
 from qiboml.backends.jax import JaxBackend
 from qiboml.models.decoding import QuantumDecoding
+from qiboml.models.encoding import BinaryEncoding
 
 
 class PSR:
@@ -58,20 +59,36 @@ class Jax:
         self._training: Circuit = None
         self._decoding: QuantumDecoding = None
         self._argnums: list[int] = None
+        self._circuit = None
 
     def evaluate(self, x: ndarray, encoding, training, decoding, backend, *parameters):
+        binary = isinstance(encoding, BinaryEncoding)
         x = backend.to_numpy(x)
         x = self._jax.cast(x, self._jax.precision)
         if self._argnums is None:
             self._argnums = range(len(parameters) + 1)
-            setattr(self, "_jacobian", jax.jacfwd(self._run, self._argnums))
+            setattr(self, "_jacobian", jax.jit(jax.jacfwd(self._run, self._argnums)))
+            setattr(
+                self,
+                "_jacobian_without_inputs",
+                jax.jit(jax.jacfwd(self._run_without_inputs, self._argnums[:-1])),
+            )
         parameters = backend.to_numpy(list(parameters))
         parameters = self._jax.cast(parameters, parameters.dtype)
-        self._encoding = encoding
-        self._training = training
+        if binary:
+            self._circuit = encoding(x) + training
+        else:
+            self._encoding = encoding
+            self._training = training
         self._decoding = decoding
         self._decoding.set_backend(self._jax)
-        gradients = self._jacobian(x, *parameters)
+        if binary:
+            gradients = (
+                self._jax.numpy.zeros((decoding.output_shape[-1], x.shape[-1])),
+                self._jacobian_without_inputs(*parameters),
+            )
+        else:
+            gradients = self._jacobian(x, *parameters)
         decoding.set_backend(backend)
         return [
             backend.cast(self._jax.to_numpy(grad).tolist(), backend.precision)
@@ -82,6 +99,10 @@ class Jax:
         circuit = self._encoding(x) + self._training
         circuit.set_parameters(parameters)
         return self._decoding(circuit)
+
+    def _run_without_inputs(self, *parameters):
+        self._circuit.set_parameters(parameters)
+        return self._decoding(self._circuit)
 
 
 def parameter_shift(