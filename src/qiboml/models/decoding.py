0750a0a03959c993676439e2ba393aa2dd480fc8
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2025-01-24 15:34:29 +0100

    fix: pylint disable

--- src/qiboml/models/decoding.py
+++ src/qiboml/models/decoding.py
@@ -104,9 +104,9 @@ class State(QuantumDecoding):
         state = super().__call__(x).state()
         return self.backend.np.vstack(  # pylint: disable=no-member
             (
-                self.backend.np.real(state),
-                self.backend.np.imag(state),
-            )  # pylint: disable=no-member
+                self.backend.np.real(state),  # pylint: disable=no-member
+                self.backend.np.imag(state),  # pylint: disable=no-member
+            )
         ).reshape(self.output_shape)
 
     @property
