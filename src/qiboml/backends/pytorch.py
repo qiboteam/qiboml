d6de0c19345e817ed4c1a220f3f764a0c8ad9a29
Renato Mello <renato.msf@gmail.com>
2025-10-18 08:55:34 +0400

    fix `random_integers`

--- src/qiboml/backends/pytorch.py
+++ src/qiboml/backends/pytorch.py
@@ -189,6 +189,10 @@ class PyTorchBackend(Backend):
         return self.copy(array[indices])
 
     def random_integers(self, low, high=None, size=None, seed=None):
+        if high is None:
+            high = low
+            low = 0
+
         if seed is not None:
             local_state = self.default_rng(seed) if isinstance(seed, int) else seed
 
