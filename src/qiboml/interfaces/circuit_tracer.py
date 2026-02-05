8d52cf31d04237d682a7cd16b4db0f45ecc0c8d3
Renato Mello <renato.msf@gmail.com>
2026-02-05 19:53:01 +0400

    fix

--- src/qiboml/interfaces/circuit_tracer.py
+++ src/qiboml/interfaces/circuit_tracer.py
@@ -388,7 +388,7 @@ class CircuitTracer(ABC):
         for circ in self.circuit_structure:
             if isinstance(circ, QuantumEncoding):
                 if x is None:
-                    x = self.engine.zeros(circ.nqubits, dtype=np.float64)
+                    x = self.engine.zeros(circ.nqubits)
                 nparams = 0
                 circ = self._build_from_encoding(circ, x, trace=False)
             elif isinstance(circ, Circuit):
