cb7909b1bb6493f0a15f45011c9ace81a0d9fd92
André Juan <andre.jfmdm@gmail.com>
2026-06-05 11:27:32 +0400

    update tests for hw backend

--- tests/test_models_optimizers.py
+++ tests/test_models_optimizers.py
@@ -176,8 +176,11 @@ def test_egt_cg(
     assert rel_errors[-1] < chem_acc
 
 
-def _loss_func_expval(circuit, backend, *, hamiltonian):
-    psi = backend.execute_circuit(circuit).state()
+def _loss_func_expval(circuit, backend, *, hamiltonian, weight=None):
+    if backend.name == "hamming_weight":
+        psi = backend.execute_circuit(circuit, weight=weight).state()
+    else:
+        psi = backend.execute_circuit(circuit).state()
     platform = backend.platform
     if platform == "tensorflow":
         if "cpu" in backend.device.lower():
@@ -188,10 +191,12 @@ def _loss_func_expval(circuit, backend, *, hamiltonian):
             psi_col = backend.reshape(psi, (-1, 1))
             h_psi = backend.matmul(backend.engine.sparse.to_dense(hamiltonian), psi_col)
             h_psi = backend.reshape(h_psi, (-1,))
+
     elif platform == "pytorch":
         h_psi = backend.engine.sparse.mm(hamiltonian, psi.unsqueeze(1)).squeeze(1)
     else:
         h_psi = hamiltonian @ psi
+
     return backend.real(backend.sum(backend.conj(psi) * h_psi))
 
 
